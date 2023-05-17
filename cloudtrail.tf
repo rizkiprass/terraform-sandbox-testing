73c8fcc368e9a39204c8a6b4fda7ca32256e8975
rizkiprass <rizkiprass@gmail.com>
2023-05-17 18:31:08 +0700

    add ct

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,22 +1,14 @@
-#module "cloudtrail" {
-#  source = "cloudposse/cloudtrail/aws"
-#  # Cloud Posse recommends pinning every module to a specific version
-#  version                       = "0.23.0"
-#  namespace                     = "test"
-#  stage                         = "dev"
-#  name                          = "cluster"
-#  enable_log_file_validation    = true
-#  include_global_service_events = true
-#  is_multi_region_trail         = false
-#  enable_logging                = true
-#  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
-#}
-#
-#module "cloudtrail_s3_bucket" {
-#  source = "cloudposse/cloudtrail-s3-bucket/aws"
-#  # Cloud Posse recommends pinning every module to a specific version
-#  version   = "0.23.0"
-#  namespace = "eg"
-#  stage     = "dev"
-#  name      = "cluster"
-#}
+module "cloudtrail" {
+      source                        = "clouddrove/cloudtrail/aws"
+      version                       = "1.3.0"
+
+      name                          = "test"
+      environment                   = "poc"
+      label_order                   = ["name", "environment"]
+      s3_bucket_name                = "test-poc-mgmt-trail"
+      enable_logging                = true
+      enable_log_file_validation    = true
+      include_global_service_events = true
+      is_multi_region_trail         = false
+      log_retention_days            = 90
+    }
