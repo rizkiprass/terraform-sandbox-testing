c821d0c5cc367ce056812650cdcfb006f01f9965
rizkiprass <rizkiprass@gmail.com>
2023-05-17 16:57:59 +0700

    delete azc

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "20.0.0.0/16"
 
   azs             = ["${var.region}a", "${var.region}b"]
-  private_subnets = ["20.0.1.0/24", "20.0.2.0/24", "20.0.3.0/24"]
-  public_subnets  = ["20.0.101.0/24", "20.0.102.0/24", "20.0.103.0/24"]
+  private_subnets = ["20.0.1.0/24", "20.0.2.0/24"]
+  public_subnets  = ["20.0.101.0/24", "20.0.102.0/24"]
 #  database_subnets    = ["20.0.21.0/24", "20.0.22.0/24"]
 
   enable_nat_gateway = true
