e1cb9235d41fd1c282d50f10d7fc6150724eb5de
rizkiprass <rizkiprass@gmail.com>
2023-05-17 16:55:34 +0700

    delete db subnet

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs             = ["${var.region}a", "${var.region}b"]
   private_subnets = ["20.0.1.0/24", "20.0.2.0/24", "20.0.3.0/24"]
   public_subnets  = ["20.0.101.0/24", "20.0.102.0/24", "20.0.103.0/24"]
-  intra_subnets    = ["20.0.21.0/24", "20.0.22.0/24"]
+#  database_subnets    = ["20.0.21.0/24", "20.0.22.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
