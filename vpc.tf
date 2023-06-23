8bdd2d832ac3c8dfb502f4cca08dfdc7bf2184a1
rizkiprass <rizkiprass@gmail.com>
2023-06-23 19:09:41 +0700

    add db subnet

--- vpc.tf
+++ vpc.tf
@@ -61,14 +61,31 @@ resource "aws_eip" "eip-nat-sandbox" {
 #}
 
 #
-#resource "aws_subnet" "subnet-db-1a" {
-# vpc_id      = module.vpc.vpc_id
-#  cidr_block = var.Private_Intra_AZ1
-#  availability_zone = format("%sa", var.aws_region)
-#}
-#
-#resource "aws_subnet" "subnet-db-1b" {
-# vpc_id      = module.vpc.vpc_id
-#  cidr_block = var.Private_Intra_AZ2
-#  availability_zone = format("%sb", var.aws_region)
-#}
+resource "aws_subnet" "subnet-db-1a" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.Private_Intra_AZ1
+  availability_zone = format("%sa", var.aws_region)
+}
+
+resource "aws_subnet" "subnet-db-1b" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.Private_Intra_AZ2
+  availability_zone = format("%sb", var.aws_region)
+}
+
+resource "aws_route_table" "data-rt" {
+  vpc_id = module.vpc.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.natgw_ids
+  }
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-%s-data-rt", var.customer, var.environment)
+  })
+}
+
+resource "aws_route_table_association" "rt-subnet-assoc-data-3a" {
+  subnet_id      = aws_subnet.subnet-db-1a
+  route_table_id = aws_route_table.data-rt
+}
