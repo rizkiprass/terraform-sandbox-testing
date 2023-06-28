f5e41e465615368c9181e893b6b4d0d96f6d5534
rizkiprass <rizkiprass@gmail.com>
2023-06-28 16:19:44 +0700

    edit name cmk and depends on

--- change-to-cmk.tf
+++ change-to-cmk.tf
@@ -1,13 +1,13 @@
 resource "aws_ami_copy" "test" {
   name              = aws_ami_from_instance.example.name
-  description       = "A copy of ami-${aws_ami_from_instance.example.id}-cmk"
+  description       = "A copy of ${aws_ami_from_instance.example.id} (cmk)"
   source_ami_id     = aws_ami_from_instance.example.id
   source_ami_region = "us-west-2"
   encrypted = "true"
   kms_key_id = "arn:aws:kms:us-west-2:272547513321:key/2b4ec77f-9419-4d5b-83a3-1b1eef55605a"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "ami-name"
   }
 
   depends_on = [aws_ami_from_instance.example]
