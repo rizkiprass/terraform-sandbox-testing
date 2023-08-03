119e3aabb30de4caf40c8c101cf3226a74439709
Rizki Prasetya <rizkiprass@gmail.com>
2023-08-03 17:19:27 +0700

    create manual ec2

--- ec2-simple.tf
+++ ec2-simple.tf
@@ -1,24 +1,33 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu_20.id
-  instance_type = "t3.micro"
-  key_name      = "pras-vivo-key"
-
-
+resource "aws_instance" "web-app" {
+  ami                         = data.aws_ami.ubuntu_20.id
+  instance_type               = "t3.micro"
+  associate_public_ip_address = "false"
+  key_name                    = "pras-vivo-key"
+  subnet_id                   = module.vpc.public_subnets[0]
+  iam_instance_profile        = aws_iam_instance_profile.ssm-profile.name
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
   root_block_device {
-
+    volume_size           = 256
+    volume_type           = "gp3"
+    iops                  = 3000
+    encrypted             = true
+    delete_on_termination = true
     tags = merge(local.common_tags, {
-      Name = "test-root-ebs"
+        Name = format("%s-%s-web-ebs", var.customer, var.environment)
     })
   }
 
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    tags = merge(local.common_tags, {
-      Name = "test-ebs-block"
-    })
+  lifecycle {
+    prevent_destroy = true
   }
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  tags = merge(local.common_tags, {
+    Name                = format("%s-%s-bastion", var.customer, var.environment),
+    OS                  = "Centos",
+    Backup              = "DailyBackup" # TODO: Set Backup Rules
+  })
 }
